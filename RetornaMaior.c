181e2ce2b1654f72b321bc9f76e8d38ad97e33f2
João Pedro Brasil <116403431+JoaoPBrasil@users.noreply.github.com>
2023-06-15 15:55:41 -0300

    Create RetornaMaior.c

---
+++ RetornaMaior.c
@@ -0,0 +1,24 @@
+#Include <stdio.h>
+
+int Maior(int x, int y);
+
+int main()
+{
+	int n1, n2, n3;
+	
+	printf("Insira os dois valores para retornarmos qual é o maior: ")
+	scanf("%d %d", &n1, &n2);
+	
+	maior = returnMaior(n1, n2);
+	printf("O maior valor entre os dois é: %d", maior);
+	
+	return 0
+}
+
+int returnMaior(int x, int y)
+{
+	if (a > b)
+		return a;
+	else 
+		return b;
+}
